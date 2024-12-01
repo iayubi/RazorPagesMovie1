893e8032c81fa3521fb4b5bb8221d7f29d256b71
ibrahin.ayubi <iayubi1@live.spcollege.edu>
2024-11-30 23:22:10 -0500

    Add search

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
@@ -21,9 +22,29 @@ namespace RazorPagesMovie.Pages_Movies
 
         public IList<Movie> Movie { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Genres {get; set; }
+
+        [BindProperty(SupportsGet =  true)]
+        public string? MovieGenre { get; set; }
         public async Task OnGetAsync()
         {
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+            //using System.Linq;
+            var movies = from m in _context.Movie  
+                        select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
             Movie = await _context.Movie.ToListAsync();
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
         }
     }
 }
